a79beb02ba35ab588739ff8b0001f3e79fb8d708
Aguiar <beca.cout@gmail.com>
2023-03-23 09:14:56 -0300

    modelando a ViewModel

---
+++ FoodiesMVC/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FoodiesMVC.Models;
+
+namespace FoodiesMVC.ViewModels
+{
+    public class LancheListViewModel
+    {
+        public List<Lanche> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}
